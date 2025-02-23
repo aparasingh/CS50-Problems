2ee6f9841ea9a22738d95c4e1b5e0e2cb40e7a6e
Singh, Aparajita <apara.singh09@gmail.com>
2025-02-23 14:36:30 +0000

    TODO: Complete dictionary.c

---
+++ Week 5/speller/dictionary.c
@@ -0,0 +1,82 @@
+// Implements a dictionary's functionality
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "dictionary.h"
+
+// Represents a node in a hash table
+typedef struct node
+{
+    char word[LENGTH + 1];
+    struct node *next;
+} node;
+
+// Choose number of buckets in hash table
+const unsigned int N = 26;
+
+// Hash table
+node *table[N];
+
+// Returns true if word is in dictionary, else false
+bool check(const char *word)
+{
+    // TODO
+    return false;
+}
+
+// Hashes word to a number
+unsigned int hash(const char *word)
+{
+    // Improve this hash function
+    return toupper(word[0]) - 'A';
+}
+
+// Loads dictionary into memory, returning true if successful, else false
+bool load(const char *dictionary)
+{
+    FILE *file = fopen(dictionary, "r");
+    if (file == NULL)
+    {
+        printf("Could not open %s.\n", dictionary);
+        return false;
+    }
+    char *word = NULL;
+    int index = 0;
+    while (fscanf(file, "%s", word) != EOF)
+    {
+        node *n = malloc(sizeof(node));
+        if(n == NULL)
+        {
+            printf("Could not allocate enough memory");
+            return false;
+        }
+        strcpy(n->word, word);
+        index = hash(word);
+        node *ptr = table[index]->next;
+        table[index]->next = n;
+        if (ptr != NULL)
+        {
+            n->next = ptr;
+
+        }
+    }
+    return true;
+}
+
+// Returns number of words in dictionary if loaded, else 0 if not yet loaded
+unsigned int size(void)
+{
+    // TODO
+    return 0;
+}
+
+// Unloads dictionary from memory, returning true if successful, else false
+bool unload(void)
+{
+    // TODO
+    return false;
+}
